25f9a237957120f0bc3924c98161e33fb9867205
David Ferguson <ferguson.david@gmail.com>
2012-03-30 13:56:40 -0700

    update build description for this full ICS build

--- device/samsung/skyrocket/ev.mk
+++ device/samsung/skyrocket/ev.mk
@@ -12,11 +12,11 @@ PRODUCT_BRAND := Samsung
 PRODUCT_DEVICE := skyrocket
 PRODUCT_MODEL := SGH-I727
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 BUILD_ID=GRI40 BUILD_FINGERPRINT=samsung/SGH-I727/SGH-I727:2.3.5/GINGERBREAD/UCKJ2:user/release-keys PRIVATE_BUILD_DESC="unkown still"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 BUILD_ID=GRI40 BUILD_FINGERPRINT=samsung/SGH-I727/SGH-I727:4.0.4/ICS/UCALC4:user/release-keys PRIVATE_BUILD_DESC="unkown still"
 
 # Set up the product codename, build version & MOTD.
-PRODUCT_CODENAME := xiphias
-PRODUCT_VERSION_DEVICE_SPECIFIC := p4
+PRODUCT_CODENAME := trainwreck
+PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Samsung Skyrocket\n------------------------------------------------\n"
 
