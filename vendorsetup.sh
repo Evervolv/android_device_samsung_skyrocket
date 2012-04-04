ebf6d6af72b161eb30acfa5533244ea2d94db132
Paul Mongold <pmongold@gmail.com>
2012-04-03 21:17:55 -0400

    skyrocket: remove lunch combo here - will use ev_skyrocket-eng from vendor/ev
    Change-Id: If518200d2ae67f12bbdec8ec027c06166096a69d

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,5 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo full_SGH-skyrocket-userdebug
+#add_lunch_combo full_SGH-skyrocket-userdebug
 #add_lunch_combo full_SGH-skyrocket-eng
