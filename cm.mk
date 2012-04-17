af8d9b14cd28a3d39cc42929d825434f6702a0ef
David Ferguson <ferguson.david@gmail.com>
2012-04-17 11:43:40 -0700

    use 480x800 boot animation

--- cm.mk
+++ cm.mk
@@ -10,7 +10,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_skyrocket BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.0.3/IML74K/107:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.0.3 IML74K 107 release-keys"
 
-TARGET_BOOTANIMATION_NAME := vertical-540x960
+TARGET_BOOTANIMATION_NAME := vertical-480x800
 
 PRODUCT_NAME := cm_skyrocket
 PRODUCT_DEVICE := skyrocket
