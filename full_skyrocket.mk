496e1a00dbc161c565288b3192877a4fac3f936f
David Ferguson <ferguson.david@gmail.com>
2012-04-17 15:26:27 -0700

    changed PRODUCT_* to match stock

--- full_skyrocket.mk
+++ full_skyrocket.mk
@@ -38,6 +38,6 @@ $(call inherit-product, device/samsung/skyrocket/device.mk)
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_skyrocket
 PRODUCT_DEVICE := skyrocket
-PRODUCT_BRAND := Samsung
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := Galaxy SII Skyrocket
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SAMSUNG-SGH-I727
