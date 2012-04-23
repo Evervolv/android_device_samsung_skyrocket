83aa554681d41e86b2cd6386976e0c61f578aef8
David Ferguson <ferguson.david@gmail.com>
2012-04-23 09:10:58 -0700

    cleaned up texts

--- cm.mk
+++ cm.mk
@@ -1,6 +1,6 @@
 $(call inherit-product, device/samsung/skyrocket/full_skyrocket.mk)
 
-PRODUCT_RELEASE_NAME := SamsungSGH-T727
+PRODUCT_RELEASE_NAME := SamsungSGH-I727
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_skyrocket BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.0.3/IML74K/107:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.0.3 IML74K 107 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_skyrocket BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.0.4/IML74K/107:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.0.4 IML74K 107 release-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-480x800
 
