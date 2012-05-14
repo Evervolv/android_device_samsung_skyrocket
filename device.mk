9740a4976638525021885a0c66c3fa6c4b384e3c
David Ferguson <ferguson.david@gmail.com>
2012-05-14 13:15:12 -0400

    add vold.fstab from quincyatt; move out of msm8660-common

--- device.mk
+++ device.mk
@@ -76,6 +76,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/samsung/skyrocket/firmware/bcm4330B1.hcd:system/etc/firmware/bcm4330B1.hcd
 
+# BT firmware
+PRODUCT_COPY_FILES += \
+    device/samsung/skyrocket/vold.fstab:system/etc/vold.fstab
+
 # common msm8660
 $(call inherit-product, device/samsung/msm8660-common/msm8660.mk)
 
