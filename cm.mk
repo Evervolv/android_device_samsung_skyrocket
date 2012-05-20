de30cc0cd572be11a4f06167e0bc642cc1169f60
Steve Kondik <shade@chemlab.org>
2012-05-20 05:56:17 -0700

    skyrocket: Fix include order
    Change-Id: I922408a71781a712ded5795829fea6e7a5ac6e2a

--- cm.mk
+++ cm.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/samsung/skyrocket/full_skyrocket.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_skyrocket BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.0.4/IML74K/107:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.0.4 IML74K 107 release-keys"
 
