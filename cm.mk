0653fa320847ae4dd15988058ef56162306da4f9
David Ferguson <ferguson.david@gmail.com>
2012-05-21 07:51:15 -0400

    skyrocket: Fix fingerprint
    Change-Id: Id579d0e97373a325b84cfcb50b0ec3cdac82fc66

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/gsm.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_skyrocket BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.0.4/IML74K/107:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.0.4 IML74K 107 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=samsung_skyrocket BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.0.4/IMM76D/UCLE2:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.0.4 IMM76D UCLE2 release-keys"
 
 TARGET_BOOTANIMATION_NAME := vertical-480x800
 
