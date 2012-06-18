580c942fb9f8c910ef483fac6c667f7291ac994e
Steve Kondik <shade@chemlab.org>
2012-06-17 18:21:16 -0700

    skyrocket: Enable enhanced NFC
    Change-Id: Ibb4e2f7a5e57f4002b35602edb1626ae5024830b

--- cm.mk
+++ cm.mk
@@ -3,6 +3,9 @@ $(call inherit-product, device/samsung/skyrocket/full_skyrocket.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
