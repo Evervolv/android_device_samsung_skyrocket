0f4bc28d6fc0bb42adcc65a115e0262bb0984a2a
David Ferguson <ferguson.david@gmail.com>
2012-10-09 18:21:35 -0400

    skyrocket: move common files to new celox-common repo
    Change-Id: I0a568b02d67f6afdfa32179947f844a1e392331e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2009 The Android Open Source Project
+# Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,8 +25,8 @@
 # against the traditional rules of inheritance).
 USE_CAMERA_STUB := true
 
-# inherit from common msm8660
--include device/samsung/msm8660-common/BoardConfigCommon.mk
+# inherit from common celox
+-include device/samsung/celox-common/BoardConfigCommon.mk
 
 # inherit from the proprietary version
 -include vendor/samsung/skyrocket/BoardConfigVendor.mk
@@ -39,10 +39,6 @@ TARGET_OTA_ASSERT_DEVICE := SGH-I727,skyrocket
 # Kernel
 TARGET_KERNEL_CONFIG        := cyanogenmod_skyrocket_defconfig
 TARGET_KERNEL_SOURCE        := kernel/samsung/msm8660-common
-BOARD_KERNEL_CMDLINE        := androidboot.hardware=qcom usb_id_pin_rework=true no_console_suspend=true zcache
-BOARD_KERNEL_BASE           := 0x40400000
-BOARD_KERNEL_PAGESIZE       := 2048
-BOARD_FORCE_RAMDISK_ADDRESS := 0x41800000
 
 # cat /proc/emmc
 #dev:        size     erasesize name
@@ -58,21 +54,4 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776192
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 838860800
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 20044333056
 BOARD_FLASH_BLOCK_SIZE := 131072
-
-BOARD_HAS_NO_SELECT_BUTTON := true
-
-# Suppress the WIPE command since it can brick our EMMC
-BOARD_SUPPRESS_EMMC_WIPE := true
-
-# Workaround for glitches while cropping bypass layers
-# TODO (orphaned) TARGET_NO_BYPASS_CROPPING := true
-
-# MTP
-# TODO (orphaned) BOARD_MTP_DEVICE := "/dev/mtp_usb"
-
-# Disable initlogo, Samsungs framebuffer is weird
-TARGET_NO_INITLOGO := true
-
-# Preload the boot animation to avoid jerkiness
-TARGET_BOOTANIMATION_PRELOAD := true
 
--- device.mk
+++ device.mk
@@ -14,53 +14,21 @@
 # limitations under the License.
 #
 
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-## The gps config appropriate for this device
-PRODUCT_COPY_FILES += device/common/gps/gps.conf_US_SUPL:system/etc/gps.conf
-
 ## (2) Also get non-open-source specific aspects if available
 $(call inherit-product-if-exists, vendor/samsung/skyrocket/skyrocket-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/skyrocket/overlay
 
-# Device uses high-density artwork where available
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
-
-# Hardware
-PRODUCT_COPY_FILES += \
-        frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
-
 # Ramdisk
 PRODUCT_COPY_FILES += \
-    device/samsung/skyrocket/ramdisk/init.qcom.rc:root/init.qcom.rc \
-    device/samsung/skyrocket/ramdisk/init.qcom.sh:root/init.qcom.sh \
     device/samsung/skyrocket/ramdisk/init.qcom.usb.rc:root/init.qcom.usb.rc \
-    device/samsung/skyrocket/ramdisk/init.qcom.usb.sh:root/init.qcom.usb.sh \
-    device/samsung/skyrocket/ramdisk/init.target.rc:root/init.target.rc \
-    device/samsung/skyrocket/ramdisk/ueventd.rc:root/ueventd.rc \
-    device/samsung/skyrocket/ramdisk/init.emmc.rc:root/init.emmc.rc \
-    device/samsung/skyrocket/fstab.qcom:root/fstab.qcom
 
 # BT firmware
 PRODUCT_COPY_FILES += \
     device/samsung/skyrocket/firmware/bcm4330B1.hcd:system/etc/firmware/bcm4330B1.hcd
 
-# Vold
-PRODUCT_COPY_FILES += \
-    device/samsung/skyrocket/vold.fstab:system/etc/vold.fstab
-
-# QRNGD
-PRODUCT_PACKAGES += qrngd
-
-# common msm8660
-$(call inherit-product, device/samsung/msm8660-common/msm8660.mk)
-
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+# Inherit from celox-common
+$(call inherit-product, device/samsung/celox-common/celox-common.mk)
 
 $(call inherit-product-if-exists, vendor/samsung/skyrocket/skyrocket-vendor.mk)
-
-#WIFI_BAND := 802_11_ABG
-#$(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/firmware/bcm4330/device-bcm.mk)
