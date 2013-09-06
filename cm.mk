2296431b82279c5b9b24f17313ad67d254d3b0c3
Dave Carey <carvsdriver@gmail.com>
2013-09-06 07:32:54 -0700

    device:skyrocket: update build fingerprint
     - Update build fingerprint to 4.3
     - Remove legacy bootanimation setting
    
    Change-Id: Ie8a73b41276b477a61015380186c38e1ec517f77

--- cm.mk
+++ cm.mk
@@ -9,9 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.1.2/IMM76D/UCLI3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.1.2 IMM76D UCLI3 release-keys"
-
-TARGET_BOOTANIMATION_NAME := 480
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.3/IMM76D/UCMC1:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.3 IMM76D UCMC1 release-keys"
 
 PRODUCT_NAME := cm_skyrocket
 PRODUCT_DEVICE := skyrocket
