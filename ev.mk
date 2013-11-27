f0bea4bc5f697e85af02a9e3437292d12771b8c1
Kiltnar <tyler.s.irwin@gmail.com>
2013-11-26 21:35:48 -0600

    Updated for Merge into Evervolv Git. Removed cm.mk, and updated ev.mk to kitkat and added newer message for MOTD.

--- ev.mk
+++ ev.mk
@@ -12,16 +12,20 @@ PRODUCT_BRAND := Samsung
 PRODUCT_DEVICE := skyrocket
 PRODUCT_MODEL := SGH-I727
 PRODUCT_MANUFACTURER := Samsung
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 BUILD_ID=GRI40 BUILD_FINGERPRINT=samsung/SGH-I727/SGH-I727:4.0.4/ICS/UCALC4:user/release-keys PRIVATE_BUILD_DESC="unkown still"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I727 TARGET_DEVICE=SGH-I727 BUILD_FINGERPRINT="samsung/SGH-I727/SGH-I727:4.4/KRT16M/UCMC1:user/release-keys" PRIVATE_BUILD_DESC="SGH-I727-user 4.4 KRT16M UCMC1 release-keys"
 
 # Set up the product codename, build version & MOTD.
 PRODUCT_CODENAME := trainwreck
 PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
-PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Samsung Skyrocket\n------------------------------------------------\n"
+PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv \nfor your Samsung Skyrocket\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at XDA-Developers.com\n------------------------------------------------\n"
 
 #PRODUCT_PACKAGES += \
     Camera
+
+# Hot reboot
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/ev/overlay/hot_reboot
 
 # Copy compatible prebuilt files
 PRODUCT_COPY_FILES +=  \
